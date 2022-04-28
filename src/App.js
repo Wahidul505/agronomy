e2b6aa1773a3b89b8171db6d981d731b2dcbbde5
Wahidul30 <wahidul505@gmail.com>
2022-04-29 00:19:26 +0600

    initially deployed in firebase and created banch of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Auth/Login/Login';
+import Register from './Pages/Auth/Register/Register';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1 className='text-center text-3xl'>Deployed</h1>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        {/* <Route path='/home' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route> */}
+      </Routes>
+      <Footer/>
     </div>
   );
 }

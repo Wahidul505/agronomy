4ce18aaacefbfbc457172c6aec352b518080698b
Wahidul30 <wahidul505@gmail.com>
2022-04-29 23:19:36 +0600

    created quantity update form and added a delivered btn in inventory/:id rounte

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -10,24 +10,34 @@ const Inventory = () => {
         .then(data => setItem(data));
     },[id]);
     return (
-        <div className='grid grid-cols-1 md:grid-cols-3'>
-            <div className='md:col-span-2 text-white md:flex gap-3'>
-                <img className='rounded-lg' src={item.image} alt="" />
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+            <div className='lg:col-span-2'>
+            <div className='text-white flex flex-col lg:flex-row gap-3 '>
+                <img className='rounded-lg w-60' src={item.image} alt="" />
                 <div className='flex flex-col justify-between'>
                     <div>
-                    <p className='text-2xl text-lime-200'>{item.name}</p>
+                    <p className='text-3xl text-lime-200'>{item.name}</p>
                     <p><small>{item.description}</small></p>
-                    <p className='text-lg'>Tk <span className='text-amber-300'>{item.price}</span></p>
-                    <p className='text-lg'>Quantity: <span className='text-amber-300'>{item.quantity}</span></p>
+                    <p className='text-lg'>Tk <span className='text-yellow-300'>{item.price}</span></p>
+                    <p className='text-lg'>Quantity: <span className='text-yellow-300'>{item.quantity}</span></p>
                     </div>
-                    <div className='border border-amber-200 rounded-lg px-2'>
-                        <p><small>Supplier: {item.supplier}</small></p>
-                        <p><small>Added by: {item.email? item.email:''}</small></p>
+                    <div className='border border-amber-200 rounded-lg pl-2'>
+                        <p><small>Supplier: <span className='text-lime-200'>{item.supplier}</span></small></p>
+                        <p><small>Added by: <span className='text-lime-200'>{item.email? item.email:''}</span></small></p>
+                        <p><small className='bg-gray-500 rounded px-1'>id:{id}</small></p>
                     </div>
                 </div>
             </div>
-            <div>
-                {/* div for updating quantity form  */}
+            <button className='rounded bg-green-600 text-white hover:text-green-600 hover:bg-white py-1 text-xl cursor-pointer px-2 mt-4 w-full lg:w-60 mb-12 md:mb-0'>Delivered</button>
+            </div>
+            <div className='bg-green-400 rounded py-2'>
+                <h1 className='text-center text-3xl text-green-700'>Restock this Item</h1>
+                <hr className='w-2/3 mx-auto'/>
+                <form className='flex flex-col mt-8 px-3'>
+                    <label className='text-lg mb-1 text-gray-700' htmlFor="quantity">Quantity of Item to Restock</label>    
+                    <input className='text-lg rounded px-1' type="number" name='quantity' id='quantity' placeholder='Enter a number of quantity' />
+                    <input className='mt-4 rounded bg-green-600 text-white hover:text-green-600 hover:bg-white py-1 text-xl cursor-pointer' type="submit" value="Restock" />
+                </form>
             </div>
         </div>
     );

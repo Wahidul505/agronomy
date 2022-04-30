9e70aa5e6432bed2ccf30464016f1782dd4d6f42
Wahidul30 <wahidul505@gmail.com>
2022-05-01 01:08:53 +0600

    implemented logic for updating and decreasing quantity of an item

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,14 +1,56 @@
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import { Link, useParams } from 'react-router-dom';
 
 const Inventory = () => {
     const { id } = useParams();
     const [item, setItem] = useState({});
+    const [quantity, setQuantity] = useState(0);
     useEffect(() => {
         fetch(`http://localhost:5000/item/${id}`)
             .then(res => res.json())
-            .then(data => setItem(data));
+            .then(data => {
+                setItem(data);
+            });
     }, [id]);
+
+    useEffect(() => {
+        setQuantity(item.quantity);
+    }, [item])
+
+    const handleDelivered = id => {
+        fetch(`http://localhost:5000/item/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ quantity: quantity - 1 })
+        }).then(res => res.json()).then(data => {
+            setQuantity(quantity - 1);
+        });
+    };
+
+    const handleRestock = (e, id) => {
+        e.preventDefault();
+        if (parseInt(e.target.quantity.value) < 0) {
+            toast.error('Please Provide Positive Quantity', { id: 'quantityError' })
+            return;
+        }
+        else if (parseInt(e.target.quantity.value) > 20) {
+            toast.error("You can't Add more than 20 at a time", { id: 'quantityError' })
+            return;
+        }
+        const totalQuantity = parseInt(quantity) + parseInt(e.target.quantity.value);
+        fetch(`http://localhost:5000/item/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ quantity: totalQuantity })
+        }).then(res => res.json()).then(data => {
+            setQuantity(totalQuantity);
+        })
+    }
     return (
         <div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
@@ -20,13 +62,13 @@ const Inventory = () => {
                                 <p className='text-3xl text-lime-200'>{item.name}</p>
                                 <p><small>{item.description}</small></p>
                                 <p className='text-lg'>Tk <span className='text-yellow-300'>{item.price}</span></p>
-                                <p className='text-lg'>Quantity: <span className='text-yellow-300'>{item.quantity}</span></p>
+                                <p className='text-lg'>Quantity: <span className='text-yellow-300'>{quantity}</span></p>
                             </div>
                             <div>
                                 <p><small>Supplier: <span className='text-lime-200'>{item.supplier}</span></small></p>
                                 <p><small>Added by: <span className='text-lime-200'>{item.email ? item.email : ''}</span></small></p>
                                 <p><small className='bg-gray-500 rounded px-1'>id:{id}</small></p>
-                                <button className='rounded bg-green-600 text-white hover:text-green-600 hover:bg-white text-xl cursor-pointer p-2 mt-4 w-full lg:w-60 mb-12 md:mb-0'>Delivered</button>
+                                <button onClick={() => handleDelivered(item._id)} className='rounded bg-green-600 text-white hover:text-green-600 hover:bg-white text-xl cursor-pointer p-2 mt-4 w-full lg:w-60 mb-12 md:mb-0'>Delivered</button>
                             </div>
                         </div>
                     </div>
@@ -34,7 +76,7 @@ const Inventory = () => {
                 <div className='border-2 border-green-600 rounded-lg py-2'>
                     <h1 className='text-center text-3xl text-green-600'>Restock this item</h1>
                     <hr className='w-2/3 mx-auto' />
-                    <form className='flex flex-col mt-8 px-3'>
+                    <form onSubmit={(e) => handleRestock(e, item._id)} className='flex flex-col mt-8 px-3'>
                         <label className='text-lg mb-1 text-gray-300' htmlFor="quantity">Quantity of Item to Restock</label>
                         <input className='text-xl rounded p-2' type="number" name='quantity' id='quantity' placeholder='Enter a number of quantity' />
                         <input className='mt-4 p-2 rounded bg-green-600 text-white hover:text-green-600 hover:bg-white text-xl cursor-pointer' type="submit" value="Restock" />
